c9f2685340fab88ff7feb6a483a82623a3d3b21c
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-02-12 15:26:43 +0530

    Correction of Halfpalindrome !

--- 1 Pattern/Palindrome Pyramid/HalfPalindrome.cpp
+++ 1 Pattern/Palindrome Pyramid/HalfPalindrome.cpp
@@ -8,41 +8,40 @@ int main()
     cin >> n;
     for (int row = 0; row < n; row = row + 1)
     {
-        for (int col = 1; col < row + 1; col++)
-        {
-            cout << col << " ";
-        }
-
-        for (int col = row - 1; col >= 1; col--) // reverse counting
-        {
-            cout << col << " ";
-        }
-        cout << endl;
-    }
-}
-
-
-//second method  full palindrome ka manupulation hai
-
-int main()
-{
-
-    int n;
-    cin >> n;
-    for (int row = 0; row < n; row = row + 1)
-    {
-        int start =  1;
         for (int col = 0; col < row + 1; col++)
         {
-            cout << start<<"";
-            start++;
+            cout << col + 1 << " ";
         }
-        //for third reverse triangle
-        int startt = row;
-        for(int col=0; col<row;col++){
-            cout<<startt;
-            startt=startt-1; //reverse counting
+
+        for (int col = row; col >= 1; col--) // reverse counting
+        {
+            cout << col << " ";
         }
         cout << endl;
     }
 }
+
+// second method  full palindrome ka manupulation hai
+
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+//     for (int row = 0; row < n; row = row + 1)
+//     {
+//         int start =  1;
+//         for (int col = 0; col < row + 1; col++)
+//         {
+//             cout << start<<"";
+//             start++;
+//         }
+//         //for third reverse triangle
+//         int startt = row;
+//         for(int col=0; col<row;col++){
+//             cout<<startt;
+//             startt=startt-1; //reverse counting
+//         }
+//         cout << endl;
+//     }
+//}
