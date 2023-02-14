3acfd85ba9874fbd21186db24a9d8db5f85d3a22
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-02-14 23:11:36 +0530

    Completed !

--- 2 Functions/SetBit.cpp
+++ 2 Functions/SetBit.cpp
@@ -3,17 +3,18 @@ using namespace std;
 
 int main()
 {
+
     int n;
-    cin >> n;   //11 => 0000101
+    cin >> n; // 11 => 0000101
     int ans = 0;
 
     while (n != 0)
     {
         if (n & 1)
         {
-            ans++; //2
+            ans++; // 2
         }
-        //right shift
+        // right shift
         n = n >> 1;
     }
 
