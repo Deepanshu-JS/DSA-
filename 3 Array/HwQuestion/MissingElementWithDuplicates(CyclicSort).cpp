1bd30392338e7c8402a2fb7e3c2542b56d96561a
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-02-23 03:11:59 +0530

    Cyclic Sort Question Missing Element from array !

---
+++ 3 Array/HwQuestion/MissingElementWithDuplicates(CyclicSort).cpp
@@ -0,0 +1,40 @@
+// Missing Element From And Array with Duplicates
+// We use cycclic Array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Dutch(vector<int> arr)
+{
+    int i = 0;
+    int n = arr.size();
+    while (i < n)
+    {
+        int index = arr[i] - 1;
+        if (arr[i] != arr[index])
+        {
+            swap(arr[i], arr[index]);
+        }
+        else
+        {
+            i++;
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] != i + 1)
+        {
+            cout << i + 1<<" ";
+        }
+    }
+}
+
+int main()
+{
+
+    vector<int> arr{1,3,3,5,3};
+
+    Dutch(arr);
+    return 0;
+}
