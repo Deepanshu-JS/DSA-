f7c72b33fae7162e5ade70a4fcfc0f7af137245f
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-03-05 03:39:19 +0530

    Divide -ive Handling !

--- 4 Searching/Class3/Divide2NumberUsingBinary.cpp
+++ 4 Searching/Class3/Divide2NumberUsingBinary.cpp
@@ -6,29 +6,32 @@ int DivideNumber(int dividend, int divisor)
 {
 
     int start = 0;
-    int end = dividend;
+    int end = abs(dividend);
     int mid = start + (end - start) / 2;
-    int ans=0;
+    int ans = 0;
     while (start <= end)
     {
-        if (mid*divisor == dividend)
+        if (abs(mid * divisor) == abs(dividend))
         {
             return mid;
         }
-        if (mid*divisor > dividend)
+        if (abs(mid * divisor) > abs(dividend))
         {
-            end= mid - 1;
+            end = mid - 1;
         }
-        if (mid*divisor < dividend)
+        if (abs(mid * divisor) < abs(dividend))     //handling -ive eith abs
         {
-            ans=mid;
-            start= mid + 1;
+            ans = mid;
+            start = mid + 1;
         }
 
-        
         mid = start + (end - start) / 2;
     }
+    if ((divisor < 0 && dividend < 0) || (divisor > 0 && dividend > 0))
     return ans;
+    else{
+        return -ans;
+    }
 }
 
 int main()
