de2a7a601777c1b5ab139facf650cd47d058eb8c
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-03-10 02:44:29 +0530

    initial

--- 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
+++ 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main(){
     vector<int>arr{12,34,67,90}
     int n= arr.size();
-    int m =2;
+    int m =2;  //no. of students
 
-    int printAllocation  = findPages()
+    int printAllocation  = findPages(arr,n,m);
+    cout<<printAllocation<<" ";
+
 }
