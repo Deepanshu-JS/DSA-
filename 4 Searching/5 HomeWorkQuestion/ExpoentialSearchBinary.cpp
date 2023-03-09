d25266f63c3da11870d788345d24d60d63eda05c
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-03-09 21:07:16 +0530

    complete exponential Search Binary !

--- 4 Searching/5 HomeWorkQuestion/ExpoentialSearchBinary.cpp
+++ 4 Searching/5 HomeWorkQuestion/ExpoentialSearchBinary.cpp
@@ -1,3 +1,56 @@
-int main(){
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binary(int arr[], int start, int end, int target)
+{
+
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        int element = arr[mid];
+        if (target == element)
+        {
+            return mid;
+        }
+
+        if (target > element)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+
+    return -1;
+}
+
+int expoSearch(int arr[], int sizen, int target)
+{
     
+    if (arr[0] == target)
+    {
+        return 0;
+    }
+    int i = 1;
+    while (i < sizen && arr[i] <= target)
+    {
+        i = i * 2;
+    }
+    return binary(arr, i / 2, min(i, sizen - 1), target);
+}
+
+int main()
+{
+    int arr[] = {3, 4, 5, 6, 11, 13, 15, 56, 70};
+    int sizen = sizeof(arr) / sizeof(int);
+    int target = 13;
+    int ans = expoSearch(arr, sizen, target);
+    cout << ans << " ";
+
+    return 0;
 }
