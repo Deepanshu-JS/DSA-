07cbf5ed7bafc2b9131e12dea17bf413d29f98cc
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-03-11 20:22:46 +0530

    initial book allocation !

--- 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
+++ 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
@@ -2,18 +2,66 @@
 #include <vector>
 using namespace std;
 
-int findPages(vector<int>arr,int n , int m){
-    int n =
+bool ispossible(int arr[], int n int m, int mid)
+{
+    int pagesum = 0;
+    int c = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > sol)
+        {
+            return false
+        }
+        if (pagesum + arr[i] > mid)
+        {
+            c++;
+            pagesum = a[i];
+            if (c > m)
+            {
+                return mid;
+            }
+        }else{
+            pagesum+=arr[i];
+        }
+        
+    }
+    return false;
 }
 
+int findPages(vector<int> arr, int n, int m)
+{
+    if (m > n)
+    {
+        return -1;
+    }
 
+    int start = 0;
+    int end = accumulate(arr, arr + n, 0);
+    int ans = -1;
 
-int main(){
-    vector<int>arr{12,34,67,90}
-    int n= arr.size();
-    int m =2;  //no. of students
+    int mid = start + (end - start) / 2;
 
-    int printAllocation  = findPages(arr,n,m);
-    cout<<printAllocation<<" ";
+    while (start <= end)
+    {
+        if (ispossible(arr, n, m, mid))
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
 
+int main()
+{
+    vector<int> arr { 12, 34, 67, 90 }
+    int n = arr.size();
+    int m = 2; // no. of students
+
+    int printAllocation = findPages(arr, n, m);
+    cout << printAllocation << " ";
 }
