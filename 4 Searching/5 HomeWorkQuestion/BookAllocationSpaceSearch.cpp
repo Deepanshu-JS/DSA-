6593a18ba64f5f22d23ef36360933e43fcb12bf0
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-03-12 03:51:01 +0530

    Problem with Book Allocation !

--- 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
+++ 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
@@ -1,49 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 
-bool ispossible(int arr[], int n int m, int mid)
+bool ispossible(vector<int> arr, int arrsize, int StudentNo, int midd)
 {
     int pagesum = 0;
-    int c = 1;
-    for (int i = 0; i < n; i++)
+    int slot = 1;
+    for (int i = 0; i < arrsize; i++)
     {
-        if (arr[i] > mid)
+        if (arr[i] > midd)
         {
-            return false
+            return false;
         }
-        if (pagesum + arr[i] > mid)
+        if (pagesum + arr[i] > midd)
         {
-            c++;
-            pagesum = a[i];
-            if (c > m)
+            slot++;
+            pagesum = arr[i];
+            if (slot > StudentNo)
             {
                 return false;
             }
-        }else{
-            pagesum+=arr[i];
         }
-        
+        else
+        {
+            pagesum += arr[i];
+        }
     }
     return false;
 }
 
-int findPages(vector<int> arr, int n, int m)
+int findPages(vector<int> arr, int arrsize, int StudentNo)
 {
-    if (m > n)
+    if (StudentNo > arrsize)
     {
         return -1;
     }
 
     int start = 0;
-    int end = accumulate(arr, arr + n, 0);
+    int end = 0;
+    for (int j = 0; j < arrsize; j++)
+    {
+        end += arr[j];
+    }
     int ans = -1;
 
     int mid = start + (end - start) / 2;
 
     while (start <= end)
     {
-        if (ispossible(arr, n, m, mid))
+        if (ispossible(arr, arrsize, StudentNo, mid))
         {
             ans = mid;
             end = mid - 1;
@@ -58,10 +64,10 @@ int findPages(vector<int> arr, int n, int m)
 
 int main()
 {
-    vector<int> arr { 12, 34, 67, 90 }
-    int n = arr.size();
-    int m = 2; // no. of students
+    vector<int> arr{12, 34, 67, 90};
+    int arrsize = arr.size();
+    int StudentNo = 2; // no. of students
 
-    int printAllocation = findPages(arr, n, m);
+    int printAllocation = findPages(arr, arrsize, StudentNo);
     cout << printAllocation << " ";
 }
