b2bad9f1b578b847ac50cc769691db78516a9175
Deepanshu Singh <singhdepanshu786@gmail.com>
2023-03-14 16:15:14 +0530

    Accumulate with Vector Complete !

--- 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
+++ 4 Searching/5 HomeWorkQuestion/BookAllocationSpaceSearch.cpp
@@ -1,3 +1,5 @@
+//Accumulate with manual array
+
 #include <iostream>
 #include <vector>
 #include <numeric>
@@ -27,7 +29,7 @@ bool ispossible(int arr[], int arrsize, int StudentNo, int midd)
             pagesum += arr[i];
         }
     }
-    return true;
+    return false;
 }
 
 int findPages(int arr[], int arrsize, int StudentNo)
@@ -70,6 +72,77 @@ int main()
 
 
 
+ // Vector With Accumulate Function
+
+ #include <iostream>
+ #include <vector>
+ #include <numeric>
+ using namespace std;
+
+bool ispossible(vector<int> arr, int arrsize, int StudentNo, int midd)
+{
+    int pagesum = 0;
+    int slot = 1;
+    for (int i = 0; i < arrsize; i++)
+    {
+        if (arr[i] > midd)
+        {
+            return false;
+        }
+        if (pagesum + arr[i] > midd)
+        {
+            slot++;
+            pagesum = arr[i];
+            if (slot > StudentNo)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            pagesum += arr[i];
+        }
+    }
+    return true;
+}
+
+int findPages(vector<int> arr, int arrsize, int StudentNo)
+{
+    if (StudentNo > arrsize)
+    {
+        return -1;
+    }
+
+    int start = 0;
+    int end = accumulate( arr.begin(), arr.begin() + arrsize, 0);
+    int ans = -1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (ispossible(arr, arrsize, StudentNo, mid))
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr{12, 34, 67, 90};
+    int arrsize = arr.size();
+    int StudentNo = 2; // no. of students
+
+    int printAllocation = findPages(arr, arrsize, StudentNo);
+    cout << printAllocation << " ";
+}
 
 
 
@@ -83,79 +156,79 @@ int main()
 
 
 
+// vector without acumulate manually adding 
 
 
-// without acumulate
-//  #include <iostream>
-//  #include <vector>
-//  #include <numeric>
-//  using namespace std;
+ #include <iostream>
+ #include <vector>
+ #include <numeric>
+ using namespace std;
 
-// bool ispossible(vector<int> arr, int arrsize, int StudentNo, int midd)
-// {
-//     int pagesum = 0;
-//     int slot = 1;
-//     for (int i = 0; i < arrsize; i++)
-//     {
-//         if (arr[i] > midd)
-//         {
-//             return false;
-//         }
-//         if (pagesum + arr[i] > midd)
-//         {
-//             slot++;
-//             pagesum = arr[i];
-//             if (slot > StudentNo)
-//             {
-//                 return false;
-//             }
-//         }
-//         else
-//         {
-//             pagesum += arr[i];
-//         }
-//     }
-//     return false;
-// }
+bool ispossible(vector<int> arr, int arrsize, int StudentNo, int midd)
+{
+    int pagesum = 0;
+    int slot = 1;
+    for (int i = 0; i < arrsize; i++)
+    {
+        if (arr[i] > midd)
+        {
+            return false;
+        }
+        if (pagesum + arr[i] > midd)
+        {
+            slot++;
+            pagesum = arr[i];
+            if (slot > StudentNo)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            pagesum += arr[i];
+        }
+    }
+    return true;
+}
 
-// int findPages(vector<int> arr, int arrsize, int StudentNo)
-// {
-//     if (StudentNo > arrsize)
-//     {
-//         return -1;
-//     }
+int findPages(vector<int> arr, int arrsize, int StudentNo)
+{
+    if (StudentNo > arrsize)
+    {
+        return -1;
+    }
 
-//     int start = 0;
-//     int end = 0;
-//     for (int j = 0; j < arrsize; j++)
-//     {
-//         end += arr[j];
-//     }
-//     int ans = -1;
+    int start = 0;
+    int end = 0;
+    for (int j = 0; j < arrsize; j++)
+    {
+        end += arr[j];
+    }
+    int ans = -1;
 
-//     while (start <= end)
-//     {
-//         int mid = start + (end - start) / 2;
-//         if (ispossible(arr, arrsize, StudentNo, mid))
-//         {
-//             ans = mid;
-//             end = mid - 1;
-//         }
-//         else
-//         {
-//             start = mid + 1;
-//         }
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (ispossible(arr, arrsize, StudentNo, mid))
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
 
-//     }
-//     return ans;
-// }
+    }
+    return ans;
+}
 
-// int main()
-// {
-//     vector<int> arr{12, 34, 67, 90};
-//     int arrsize = arr.size();
-//     int StudentNo = 2; // no. of students
+int main()
+{
+    vector<int> arr{12, 34, 67, 90};
+    int arrsize = arr.size();
+    int StudentNo = 2; // no. of students
 
-//     int printAllocation = findPages(arr, arrsize, StudentNo);
-//     cout << printAllocation << " ";
-// }
+    int printAllocation = findPages(arr, arrsize, StudentNo);
+    cout << printAllocation << " ";
+}
